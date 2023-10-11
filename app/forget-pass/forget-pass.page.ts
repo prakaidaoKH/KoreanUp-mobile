d71cf97bc492166b91ff92711f7bc1176bbce402
Prakaidao Khansamrit <138117680+prakaidaoKH@users.noreply.github.com>
2023-10-11 23:04:21 +0700

    Update forget-pass.page.ts

--- app/forget-pass/forget-pass.page.ts
+++ app/forget-pass/forget-pass.page.ts
@@ -3,6 +3,7 @@ import { MemberService } from '../services/member.service';
 import { Member } from '../models/member-model';
 import { Storage } from '@ionic/storage';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-forget-pass',
@@ -24,7 +25,8 @@ export class ForgetPassPage implements OnInit {
   constructor(
     private memberService: MemberService,
     private storage: Storage,
-    private router: Router
+    private router: Router,
+    private alert: AlertController
   ) {}
 
   async ngOnInit() {
@@ -34,6 +36,23 @@ export class ForgetPassPage implements OnInit {
   fineAccountByEmail(email: string) {
     this.memberService.forgetPassByEmail(email).subscribe(async (response) => {
       if (response.status !== 'success') {
+        return;
+      }
+      if (response.member === null) {
+        const alert = await this.alert.create({
+          header: 'ไม่พบบัญชี',
+          subHeader: '',
+          message: 'ไม่มีบัญชีจากอีเมลนี้',
+          buttons: [
+            {
+              text: 'ตกลง',
+              role: 'confirm',
+              handler: () => {},
+            },
+          ],
+        });
+
+        await alert.present();
         return;
       }
       this.member = response.member;
